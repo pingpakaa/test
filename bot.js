4fcf7b2dd28fc3f53455a87c04e36f56bf570192
pingpakaa <54412829+pingpakaa@users.noreply.github.com>
2019-08-23 01:05:45 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,17 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
- 
+const prizes = ["prize1", "prize2", "prize3"];
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    if(message.content === "!prize"){
+        message.reply("You won the following prize: \n"+prizes[Math.floor(Math.random()*prizes.length)]);
+    }
 });
 
- 
-
 // THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
